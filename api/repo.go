d26354ff03e9b8bbf985a7907c1cecbad85022e4
Erik Grinaker <erik@bengler.no>
2016-09-04 12:40:02 +0100

    Use hardcoded hook URL https://drone.sanity.io/hook.

--- api/repo.go
+++ api/repo.go
@@ -70,7 +70,7 @@ func PostRepo(c *gin.Context) {
 	}
 
 	link := fmt.Sprintf(
-		"%s/hook?access_token=%s",
+		"https://drone.sanity.io/hook?access_token=%s",
 		httputil.GetURL(c.Request),
 		sig,
 	)
